b113c9477222e991f7b160182475521ad73f3442
FCHDev <acetige@gmail.com>
2023-02-16 23:06:03 +0100

    Amélioration footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="flex justify-center items-center w-full h-[10vh] bg-jaune">
+        <div className="flex justify-center items-center w-full
+        2xl:h-[90px]
+        xl:h-[70px]
+        h-[60px]
+        bg-jaune">
             <h1 className="font-headerTitle">LE FOOTER</h1>
         </div>
     );
